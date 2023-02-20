c401bc1e7b6d0b04ddf857eaa8329dfb6c6923e1
Caio Scatolino <66438920+CaioScatolino@users.noreply.github.com>
2023-02-20 14:33:42 -0300

    Adicionando aulas

---
+++ 01-fundamentos-react/src/App.jsx
@@ -0,0 +1,21 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./style.css";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Post
+        tipo=""
+        author="Caio"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, reiciendis? Odio a voluptas nulla laudantium, quis facere quidem aliquid similique, molestias ea et magni ex quae amet. Quis, temporibus corporis!"
+      />
+      <Post
+        tipo=""
+        author="Denis"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, reiciendis? Odio a voluptas nulla laudantium, quis facere quidem aliquid similique, molestias ea et magni ex quae amet. Quis, temporibus corporis!"
+      />
+    </div>
+  );
+}
