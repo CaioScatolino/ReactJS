10dd3ee9619edec254379ba2cd3248cd638e5536
Caio Scatolino <66438920+CaioScatolino@users.noreply.github.com>
2023-02-23 15:40:54 -0300

    Atualizando aulas

--- 01-fundamentos-react/src/App.jsx
+++ 01-fundamentos-react/src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        tipo=""
-        author="Caio"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, reiciendis? Odio a voluptas nulla laudantium, quis facere quidem aliquid similique, molestias ea et magni ex quae amet. Quis, temporibus corporis!"
-      />
-      <Post
-        tipo=""
-        author="Denis"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, reiciendis? Odio a voluptas nulla laudantium, quis facere quidem aliquid similique, molestias ea et magni ex quae amet. Quis, temporibus corporis!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            tipo=""
+            author="Caio"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, reiciendis? Odio a voluptas nulla laudantium, quis facere quidem aliquid similique, molestias ea et magni ex quae amet. Quis, temporibus corporis!"
+          />
+          <Post
+            tipo=""
+            author="Denis"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, reiciendis? Odio a voluptas nulla laudantium, quis facere quidem aliquid similique, molestias ea et magni ex quae amet. Quis, temporibus corporis!"
+          />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ 01-fundamentos-react/src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://tm.ibxk.com.br/2021/08/10/10181814794027.jpg?ims=500x80"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/caioscatolino.png"
+        />
+        <strong>Caio Scatolino</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
