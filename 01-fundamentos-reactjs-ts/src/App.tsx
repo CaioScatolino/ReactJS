dd33ec4dde623f5ffc35dad3748431dfc896607f
Caio Scatolino <66438920+CaioScatolino@users.noreply.github.com>
2023-03-03 15:45:16 -0300

    Atualizando o projeto da aula 1 para typescript

---
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,71 @@
+import { Header } from "./components/Header";
+import { Post, PostType } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+import { Comment } from "./components/Comment";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/caioscatolino.png",
+      name: "Caio Scatolino",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+
+    publishedAt: new Date("2023-02-22 10:06:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Back-End Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+
+    publishedAt: new Date("2023-02-24 15:06:00"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                post = {post}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ 01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,62 @@
+import styles from "./Comment.module.css";
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => state + 1);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/caioscatolino.png"
+        alt=""
+        onClick={() => {
+          alert("Hello World");
+        }}
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Caio Scatolino</strong>
+              <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button
+              onMouseDown={handleDeleteComment}
+              title="Deletar comentário"
+            >
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onMouseDown={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
